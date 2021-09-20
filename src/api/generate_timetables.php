3ff2009db49d86944541afada3aaa522d8a6a940
Dinis Rodrigues <denis.dr4@gmail.com>
2021-09-21 00:40:30 +0100

    refactor: uses python3 now

---
+++ src/api/generate_timetables.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: text/html;charset=utf-8');
+ksort($_POST);
+
+echo "$_POST";
+$args = "";
+foreach($_POST as $x=>$x_value) {
+    $args = $args . " " . urlencode($x_value);
+}
+
+$datetime = date('d/m/Y H:i:s');
+$ip = $_SERVER['REMOTE_ADDR'];
+$command = "/usr/bin/python3 ../horarios.py $args";
+
+echo shell_exec("echo [$datetime] \\($ip\\) $command >> commands.log");
+echo shell_exec($command);
+?>

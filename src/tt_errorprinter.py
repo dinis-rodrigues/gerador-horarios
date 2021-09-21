7af99aa24d80f0b3d6544ed7de60ae443a02d587
Dinis Rodrigues <denis.dr4@gmail.com>
2021-09-21 03:18:50 +0100

    refactor: moved files

--- src/tt_errorprinter.py
+++
@@ -1,9 +0,0 @@
-class HTMLErrorPrinter:
-
-    def no_timetables(self):
-        return """\
-  <body style="margin:15px">
-    <div class="mtop1 coutput2" style="line-height: 1.5em;font-family:'Verdana';font-size:12px;color:#DD0000">
-      Não há nenhum horário compatível com todas as disciplinas que seleccionou.<br>
-      Experimente remover um tipo de aula ou uma disciplina.
-    </div>"""

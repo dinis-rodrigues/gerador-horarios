fb0a7543f0d3e523b80942d66e39463d128f2d3d
Dinis Rodrigues <denis.dr4@gmail.com>
2021-09-21 03:26:04 +0100

    feat: added visit counter

---
+++ src/client/visitCount.js
@@ -0,0 +1,33 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAHPrWvVr1El3NkJd3C0gbZbiTl_weCTlE",
+  authDomain: "tsb-aplication.firebaseapp.com",
+  databaseURL: "https://tsb-aplication.firebaseio.com",
+  projectId: "tsb-aplication",
+  storageBucket: "tsb-aplication.appspot.com",
+  messagingSenderId: "124968779478",
+  appId: "1:124968779478:web:0a2c6266560c594a779377",
+  measurementId: "G-0Z77DRSCH6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Database
+var db = firebase.database();
+
+// Retrieve visit count on every page entry/refresh
+db.ref("/public/scheduleWebsite/visitCount")
+  .once("value")
+  .then((snapshot) => {
+    const visitors = snapshot.val() + 1;
+    // visual count up on index.html DOM
+    let demo = new countUp.CountUp("visits", visitors);
+    if (!demo.error) {
+      demo.start();
+    } else {
+      console.error(demo.error);
+    }
+    // Update new count on database
+    db.ref("/public/scheduleWebsite/visitCount").set(visitors);
+  });
